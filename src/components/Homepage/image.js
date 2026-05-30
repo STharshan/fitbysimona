9ab8b41b4492111fb9d16ee2eec9dd459956156e
shandiru <msddiru@gmail.com>
2026-05-30 21:42:47 +0530

    Add public images through im53

--- src/components/Homepage/image.js
+++ src/components/Homepage/image.js
@@ -3,6 +3,54 @@ export const im2 = "/im2.webp";
 export const im3 = "/im3.webp";
 export const im4 = "/im4.webp";
 export const im5 = "/im5.webp";
+export const im6 = "/im6.webp";
+export const im7 = "/im7.webp";
+export const im8 = "/im8.webp";
+export const im9 = "/im9.webp";
+export const im10 = "/im10.webp";
+export const im11 = "/im11.webp";
+export const im12 = "/im12.webp";
+export const im13 = "/im13.webp";
+export const im14 = "/im14.webp";
+export const im15 = "/im15.webp";
+export const im16 = "/im16.webp";
+export const im17 = "/im17.webp";
+export const im18 = "/im18.webp";
+export const im19 = "/im19.webp";
+export const im20 = "/im20.webp";
+export const im21 = "/im21.webp";
+export const im22 = "/im22.webp";
+export const im23 = "/im23.webp";
+export const im24 = "/im24.webp";
+export const im25 = "/im25.webp";
+export const im26 = "/im26.webp";
+export const im27 = "/im27.webp";
+export const im28 = "/im28.webp";
+export const im29 = "/im29.webp";
+export const im30 = "/im30.webp";
+export const im31 = "/im31.webp";
+export const im32 = "/im32.webp";
+export const im33 = "/im33.webp";
+export const im34 = "/im34.webp";
+export const im35 = "/im35.webp";
+export const im36 = "/im36.webp";
+export const im37 = "/im37.webp";
+export const im38 = "/im38.webp";
+export const im39 = "/im39.webp";
+export const im40 = "/im40.webp";
+export const im41 = "/im41.webp";
+export const im42 = "/im42.webp";
+export const im43 = "/im43.webp";
+export const im44 = "/im44.webp";
+export const im45 = "/im45.webp";
+export const im46 = "/im46.webp";
+export const im47 = "/im47.webp";
+export const im48 = "/im48.webp";
+export const im49 = "/im49.webp";
+export const im50 = "/im50.webp";
+export const im51 = "/im51.webp";
+export const im52 = "/im52.webp";
+export const im53 = "/im53.webp";
 
 export const transformationImages = [
   im1,
@@ -10,4 +58,52 @@ export const transformationImages = [
   im3,
   im4,
   im5,
+  im6,
+  im7,
+  im8,
+  im9,
+  im10,
+  im11,
+  im12,
+  im13,
+  im14,
+  im15,
+  im16,
+  im17,
+  im18,
+  im19,
+  im20,
+  im21,
+  im22,
+  im23,
+  im24,
+  im25,
+  im26,
+  im27,
+  im28,
+  im29,
+  im30,
+  im31,
+  im32,
+  im33,
+  im34,
+  im35,
+  im36,
+  im37,
+  im38,
+  im39,
+  im40,
+  im41,
+  im42,
+  im43,
+  im44,
+  im45,
+  im46,
+  im47,
+  im48,
+  im49,
+  im50,
+  im51,
+  im52,
+  im53,
 ];
